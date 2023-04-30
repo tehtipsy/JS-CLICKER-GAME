bc360c89c8aa081be0cc54e24a8d6ec6b1d8d698
Asaf Shmueli <asafshmueli@gmail.com>
2023-05-01 01:44:08 +0300

    fix negeative resources

--- classgame.js
+++ classgame.js
@@ -2,59 +2,75 @@ import { config } from "./config.js";
 
 class Game {
     constructor() {
-        this.resources = {
+        this.resources = { // move to config
             money: 0,
             population: 0,
             pollution: 0,
-            wood: 0,
+            wood: 100,
             coal: 0,
             energy: 0,
         };
-        this.producers = {
+        this.producers = { // move to config
             lumberjack: 0,
-            coalMine: 0,
+            coalMine: 2,
             powerPlant: 0,
         };
     };
-
+    
+    // update resources automagicly
     updateAutoProduction() {
-        this.resources.population++;
+        this.resources.population++; // SUPPLY PLACEHOLDER
     };
 
-    findProducerIndex(producer) {
+    // get producer Index from config file
+    findProducerIndex(producer) { 
         const producerIndex = config.producers.findIndex(p => p.name === producer);
         return producerIndex
     }; 
 
+    // update producers using config index
     updateAllProducers() {
         Object.keys(this.producers).forEach(producer => {
-            const producerIndex = this.findProducerIndex(producer);
-            // const producerIndex = config.producers.findIndex(p => p.name === producer);
-            // this.produce(producer);
-            // this.updateProducer(producer);
-            // this.produce(producerIndex);
-            this.updateProducer(producerIndex);
+            if (this.producers[producer] >= 1) { // ???
+                const producerIndex = this.findProducerIndex(producer);
+                this.updateProducer(producerIndex);
+            }
         });
     };
 
+    // check producer upkeep and then produce
     updateProducer(producer) {
         const success = this.consumeCosts(producer, config);
         this.produce(producer, success, config);
-        // const config = {}; // get config - ES6 import
-        // console.log(success)
+        // console.log(success) // ???
         // this.producePollution(producer, success, config); // redundent ?
     };
 
+    // get name of producer using index
+    getProducerName(producer) {
+        return Object.keys(this.producers)[producer]
+    };
+
+    // get number of producers using name
+    getNumberOfProducers(producer) {
+        return this.producers[this.getProducerName(producer)]
+    };
+
+    // fix this mess
     consumeCosts(producer, config) {
+        // get number of active producers somewhere
         let numberSucceeded = 0
         config.producers[producer].upkeepCosts.forEach(resource => {
             if (this.resources[resource.currency] >= resource.base) {
-                this.resources[resource.currency] -= resource.base // move to diffrent function ?
-                numberSucceeded++
+                console.log(this.getProducerName(producer))
+                console.log(this.getNumberOfProducers(producer))
+                this.resources[resource.currency] -= resource.base * this.getNumberOfProducers(producer) // move to diffrent function
+                numberSucceeded++ // fix this
             }
         }); return numberSucceeded
     };
-
+    
+    // produce resources times number of "active producers"
     produce(producer, numberSucceeded, config) {
         config.producers[producer].production.forEach(resource => {
             this.resources[resource.currency] += resource.base * numberSucceeded;
@@ -71,10 +87,10 @@ class Game {
 
     draw() {
         // draw ALL THE THINGS
-        // // bonuses per sec //
-        // document.getElementById("scorepersec").innerHTML = ;
-        // document.getElementById("pollutionpersec").innerHTML = ;
-        // document.getElementById("fossilfuelpersec").innerHTML = ;        
+        // // upgrade count //
+        document.getElementById("lumberjackcount").innerHTML = this.producers.lumberjack ;
+        document.getElementById("coalminecount").innerHTML = this.producers.coalMine;
+        document.getElementById("powerplantcount").innerHTML = this.producers.powerPlant;
         // // game counters //
         // document.getElementById("totalclicks").innerHTML = ;
         document.getElementById("score").innerHTML = this.resources.money;
@@ -92,6 +108,10 @@ class Game {
         document.getElementById("lumberjack").innerHTML = config.producers[0].purchaseCosts[1].base;
         // document.getElementById("coalmine").innerHTML = ;
         // document.getElementById("powerplant").innerHTML = ;
+        // // bonuses per sec //
+        // document.getElementById("scorepersec").innerHTML = ;
+        // document.getElementById("pollutionpersec").innerHTML = ;
+        // document.getElementById("fossilfuelpersec").innerHTML = ;        
         // // subUpgrade cost //
         // document.getElementById("jacksaws").innerHTML = ;
         // document.getElementById("powersaws").innerHTML = ;
@@ -102,10 +122,6 @@ class Game {
         // document.getElementById("morepower").innerHTML = ;
         // document.getElementById("evenmorepower").innerHTML = ;
         // document.getElementById("mostpower").innerHTML = ;
-        // // upgrade count //
-        document.getElementById("lumberjackcount").innerHTML = this.producers.lumberjack ;
-        document.getElementById("coalminecount").innerHTML = this.producers.coalMine;
-        document.getElementById("powerplantcount").innerHTML = this.producers.powerPlant;
         // // subUpgrade count //
         // document.getElementById("jacksawscount").innerHTML = ;
         // document.getElementById("powersawscount").innerHTML = ;
