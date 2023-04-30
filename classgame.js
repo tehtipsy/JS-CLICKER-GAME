61556aebb3679ae6bbb9442b460989bd4c31c5b9
Asaf Shmueli <asafshmueli@gmail.com>
2023-04-30 13:53:45 +0300

    do the things

--- classgame.js
+++ classgame.js
@@ -1,3 +1,4 @@
+import config from "./config";
 
 class Game {
     constructor() {
@@ -7,10 +8,12 @@ class Game {
             pollution: 0,
             wood: 0,
             coal: 0,
+            energy: 0,
         };
         this.producers = {
             lumberjack: 0,
             coalPlant: 0,
+            powerPlant: 0,
         };
     };
 
@@ -18,22 +21,25 @@ class Game {
         this.resources.population++;
     };
 
-    updateAllPRoducers() {
+    updateAllProducers() {
         Object.keys(this.producers).forEach(producer => {
             this.produce(producer);
         });
     };
 
     updateProducer(producer) {
-        const config = {}; // get config
+        // const config = {}; // get config ES6 import
         const success = this.consumeCosts(producer, config);
         this.produce(producer, config, success);
         this.producePollution(producer, config, success);
     };
 
-    consumeCosts(producer, config)
-    {
+    consumeCosts(producer, config) {
         // consume costs and return the number of "active producers"
+        // let numberSucseeded = 0
+        // config.producers[producer].upkeepCosts.forEach(resource => {
+        //     this.resources[resource.currency] -= 
+        // }); return numberSucseeded
     };
 
     produce(producer, numberSucceeded, config) {
@@ -52,12 +58,57 @@ class Game {
 
     draw() {
         // draw ALL THE THINGS
+                // // bonuses per sec //
+                // document.getElementById("scorepersec").innerHTML = ;
+                // document.getElementById("pollutionpersec").innerHTML = ;
+                // document.getElementById("fossilfuelpersec").innerHTML = ;        
+                // // game counters //
+                // document.getElementById("totalclicks").innerHTML = ;
+                // document.getElementById("score").innerHTML = ;
+                // document.getElementById("totalfossilfuel").innerHTML = ;
+                // // resource count //
+                document.getElementById("population").innerHTML = this.resources.population;
+                // document.getElementById("pollution").innerHTML = ;
+                // document.getElementById("totaltrees").innerHTML = ;
+                // document.getElementById("totalcoal").innerHTML = ;
+                // document.getElementById("totalenergy").innerHTML = ;
+                // document.getElementById("woodpersec").innerHTML = ;
+                // document.getElementById("coalpersec").innerHTML = ;
+                // document.getElementById("energypersec").innerHTML = ;
+                // // upgrade cost //
+                // document.getElementById("lumberjack").innerHTML = ;
+                // document.getElementById("coalmine").innerHTML = ;
+                // document.getElementById("powerplant").innerHTML = ;
+                // // subUpgrade cost //
+                // document.getElementById("jacksaws").innerHTML = ;
+                // document.getElementById("powersaws").innerHTML = ;
+                // document.getElementById("heavylogger").innerHTML = ;
+                // document.getElementById("moreminers").innerHTML = ;
+                // document.getElementById("evenmoreminers").innerHTML = ;
+                // document.getElementById("mineshafts").innerHTML = ;
+                // document.getElementById("morepower").innerHTML = ;
+                // document.getElementById("evenmorepower").innerHTML = ;
+                // document.getElementById("mostpower").innerHTML = ;
+                // // upgrade count //
+                // document.getElementById("lumberjackcount").innerHTML = ;
+                // document.getElementById("coalminecount").innerHTML = ;
+                // document.getElementById("powerplantcount").innerHTML = ;
+                // // subUpgrade count //
+                // document.getElementById("jacksawscount").innerHTML = ;
+                // document.getElementById("powersawscount").innerHTML = ;
+                // document.getElementById("heavymachinescount").innerHTML = ;
+                // document.getElementById("moreminerscount").innerHTML = ;
+                // document.getElementById("evenmoreminerscount").innerHTML = ;
+                // document.getElementById("mineshaftscount").innerHTML = ;
+                // document.getElementById("morepowercount").innerHTML = ;
+                // document.getElementById("evenmorepowercount").innerHTML = ;
+                // document.getElementById("mostpowercount").innerHTML = ;
     }
 
     stateUpdate() {
         this.updateAutoProduction();
-        this.updateAllPRoducers();
-        this.sellResouces();
+        // this.updateAllProducers(); // build function and config
+        // this.sellResouces(); // TBD
     };
 
     update() {
@@ -72,14 +123,14 @@ class Game {
     }
 }
 
-function addEventsToButtons(game) {
-    document.getElementById('mybutton').addEventListener(e => {
-        game.buttonPress('sexy button');
-    });
-}
+// function addEventsToButtons(game) {
+//     document.getElementById('mybutton').addEventListener(e => {
+//         game.buttonPress('sexy button');
+//     });
+// }
 
-let game = Game();
+let game = new Game();
 
-addEventsToButtons(game);
+// addEventsToButtons(game);
 
 setInterval(() => game.update(), 1000);
--- config.js
+++ config.js
@@ -26,15 +26,103 @@ const config = {
                 {
                     currency: "wood",
                     base: 1,
+                },                  
+                {
+                    currency: "money",
+                    base: 10000000000,
                 },  
-                // pollution? 
+                // pollution? population?
+                {
+                    currency: "pollution",
+                    base: 1,
+                },
+            ],
+            // pollution ?
+        },        
+        
+        {
+            name: "Coal Mine",
+            purchaseCosts: [
+                {
+                    currency: "population",
+                    currencyGrowth: [1],
+                    base: 10,
+                },
+                {
+                    currency: "money",
+                    currencyGrowth: "1.1",
+                    base: 10000,
+                },
+            ],
+            upkeepCosts: [
+                {
+                    currency: "money",
+                    base: 2500,
+                    currencyGrowth: "1",
+                },                  
+                {
+                    currency: "wood",
+                    base: 1,
+                    currencyGrowth: "1",
+                },                
+            ],
+            production: [
+                {
+                    currency: "coal",
+                    base: 100,
+                },  
+                // pollution? population?
+                {
+                    currency: "pollution",
+                    base: 100,
+                },
+            ],
+            // pollution ?
+        },        
+        
+        {
+            name: "Power Plant",
+            purchaseCosts: [
+                {
+                    currency: "population",
+                    currencyGrowth: [1],
+                    base: 10,
+                },
+                {
+                    currency: "money",
+                    currencyGrowth: "1.1",
+                    base: 200000,
+                },
+            ],
+            upkeepCosts: [
+                {
+                    currency: "money",
+                    base: 250,
+                    currencyGrowth: "1",
+                },                       
+                {
+                    currency: "coal",
+                    base: 2000,
+                    currencyGrowth: "1",
+                },                
+            ],
+            production: [
+                {
+                    currency: "wood",
+                    base: 1,
+                },  
+                // pollution? population?
+                {
+                    currency: "pollution",
+                    base: 100,
+                },
             ],
             // pollution ?
         },
 
 
         {
-            name: "coal plant",
+            name: "Coal Plant",
             purchaseCosts: [ "..." ],
             // ...
         },
