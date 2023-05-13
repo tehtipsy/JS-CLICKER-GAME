b3e862344ded0f440df9ebe8452d5a7061f4b611
Asaf Shmueli <asafshmueli@gmail.com>
2023-05-13 16:13:55 +0300

    mooved config to googlesheet

--- config.js
+++ config.js
@@ -1,193 +1,41 @@
-// async function fetchData() {
-//     try {
-//       const response = await fetch('https://example.com/data.json');
-//       if (!response.ok) {
-//         throw new Error('Network response was not OK');
-//       }
-//       const data = await response.json();
-//       // Create an object using the JSON data
-//       const myObject = {
-//         // Define properties using data from the JSON
-//         name: data.name,
-//         age: data.age,
-//         // Add any other properties as needed
-//       };
-//       return myObject;
-//     } catch (error) {
-//       // Handle any errors that occur during the fetch request
-//       console.error('Error:', error);
-//       return null;
-//     }
-//   }
-  
-//   export default fetchData;
-// insted of config import
-// import fetchData from './path/to/fetchData';
-
-// async function useFetchedData() {
-//   const myObject = await fetchData();
-//   if (myObject) {
-//     // Use the fetched data object here
-//     console.log(myObject);
-//   } else {
-//     // Handle the case when there was an error during fetch
-//     console.error('Failed to fetch data');
-//   }
-// }
-
-// useFetchedData();
-
-  
-
-export const config = {
-    resources: [
-        "money",
-        "population",
-        "pollution",
-        "wood",
-        "coal",
-        "energy",
-    ],
-    producers: [
-        {
-            name: "axe",
-            purchaseCosts: [
-            ],
-            purchaseProduction: [
-                // {
-                //     currency: "money",
-                //     currencyGrowth: 0, // make this work
-                //     base: 250,
-                // },         
-                // {
-                //     currency: "wood",
-                //     currencyGrowth: 0, // make this work
-                //     base: 1,
-                // },
-            ],
-            upkeepCosts: [    
-            ],
-            production: [
-            ],
-        },
-        {
-            name: "lumberjack",
-            purchaseCosts: [
-                {
-                    currency: "population",
-                    currencyGrowth: [1],
-                    base: 1,
-                },
-                // {
-                //     currency: "money",
-                //     currencyGrowth: "1.1",
-                //     base: 2500,
-                // },
-            ],
-            upkeepCosts: [
-                {
-                    currency: "money",
-                    base: 250,
-                    currencyGrowth: "1",
-                },               
-                // {
-                //     currency: "population",
-                //     currencyGrowth: 1,
-                //     base: 1,
-                // }, 
-            ],
-            production: [
-                {
-                    currency: "wood",
-                    base: 10,
-                },                  
-                {
-                    currency: "money",
-                    base: 1,
-                },  
-                {
-                    currency: "pollution",
-                    base: 1,
-                },
-            ],
-        },        
-        
-        {
-            name: "coalMine",
-            purchaseCosts: [
-                {
-                    currency: "population",
-                    currencyGrowth: [1],
-                    base: 10,
-                },
-                {
-                    currency: "money",
-                    currencyGrowth: "1.1",
-                    base: 10000,
-                },
-            ],
-            upkeepCosts: [
-                {
-                    currency: "money",
-                    base: 2500,
-                    currencyGrowth: "1",
-                },
-                {
-                    currency: "wood",
-                    base: 1,
-                    currencyGrowth: "1",
-                },                
-            ],
-            production: [
-                {
-                    currency: "coal",
-                    base: 100,
-                },  
-                {
-                    currency: "pollution",
-                    base: 100,
-                },
-            ],
-        },        
-        
-        {
-            name: "powerPlant",
-            purchaseCosts: [
-                {
-                    currency: "population",
-                    currencyGrowth: [1],
-                    base: 10,
-                },
-                {
-                    currency: "money",
-                    currencyGrowth: "1.1",
-                    base: 200000,
-                },
-            ],
-            upkeepCosts: [
-                {
-                    currency: "money",
-                    base: 250,
-                    currencyGrowth: "1",
-                },                       
-                {
-                    currency: "coal",
-                    base: 2000,
-                    currencyGrowth: "1",
-                },                
-            ],
-            production: [
-                {
-                    currency: "energy",
-                    base: 100,
-                },  
-                {
-                    currency: "pollution",
-                    base: 100,
-                },
-            ],
-        },
-    ],
+async function fetchData() {
+    try {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbzWaydPCzVq09m3-Eg_NBlk0QiHLPM5ny0xHSyZ4mzdO6wSWkApE9Ar1Z7KfuWaVdFcRw/exec');
+        if (!response.ok) {
+            throw new Error('Network response was not OK');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        // Handle any errors that occur during the fetch request
+        console.error('Error:', error);
+        return null;
+    }
 };
 
-// export default config;
+async function useFetchedData() {
+    try {
+        const { content } = await fetchData();
+        if (content) {
+            const config = {
+                resources: content.resources,
+                producers: content.producers.map(producer => ({
+                name: producer.name,
+                purchaseCosts: producer.purchaseCosts,
+                // purchaseProduction: [],
+                upkeepCosts: producer.upkeepCosts,
+                production: producer.production
+            }))
+        };
+        console.log('Data fetched and assigned to config:', config);
+        // Use the 'config' object as needed
+        return config
+        } else {
+            console.error('Failed to fetch data');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+export const config = await useFetchedData();
